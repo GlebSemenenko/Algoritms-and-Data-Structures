1f394a19b3c2bd3d5dddde00c9e29806e1f3c608
gleb <semenenkogleb195@gmail.com>
2026-08-02 13:28:53 +0300

    cogs

---
+++ src/livecode/Codewars/7kyu/Cogs.py
@@ -0,0 +1,18 @@
+from fractions import Fraction
+
+def cog_RPM(cogs):
+    a = Fraction(cogs[0], cogs[-1])
+    b = 1
+    if len(cogs) % 2 == 0:
+        b = -1
+    return a * b
+
+print(cog_RPM([100, 75]))
+
+
+
+
+def cv(i):
+    return i * i
+
+cv(5)
